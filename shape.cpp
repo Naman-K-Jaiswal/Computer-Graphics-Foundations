3bed37f002ee86e19c54e3233750577f24b31eb6
Priyanka <priyankapaliwal973@gmail.com>
2023-11-29 15:56:03 +0530

    This is my answer

--- shape.cpp
+++ shape.cpp
@@ -20,6 +20,20 @@ int main() {
 //   std::cout << "\nTriangle: " << std::endl;
 //   std::cout << "Area: " << triangle.calculateArea() << std::endl;
 //   std::cout << "Perimeter: " << triangle.calculatePerimeter() << std::endl;
-
+ float percircle = 2*3.14*7.0;
+ float areacircle = 3.14*7.0*7.0;
+ std::cout<<"Circle:"<<std::endl;
+ std::cout<<"Perimeter is" <<percircle<<std::endl;
+ std::cout<<"Area is "<<areacircle<<std::endl;
+ float perrectangle = (4.2+8.0)*2;
+ float arearectangle = 4.2*8.0;
+ std::cout<<"Rectangle :"<<std::endl;
+ std::cout<<"Perimeter is :"<<perrectangle<<std::endl;
+ std::cout<<"Area is :"<<arearectangle<<std::endl;
+ float pertraingle = 4.0+4.0+3.2;
+ float areatraingle = 0.5*2.0*3.7;
+ std::cout<<"Traingle :"<<std::endl;
+ std::cout<<"Perimeter is "<<pertraingle<<std::endl;
+ std::cout<<"Area is "<<areatraingle<<std::endl;
   return 0;
 }
