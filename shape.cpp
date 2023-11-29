a52fd59945122e87a28b40052a213dd81715eb9b
unknown <arohidivya16@gmail.com>
2023-11-29 16:03:12 +0530

    I have modified the assignment

--- shape.cpp
+++ shape.cpp
@@ -20,6 +20,20 @@ int main() {
 //   std::cout << "\nTriangle: " << std::endl;
 //   std::cout << "Area: " << triangle.calculateArea() << std::endl;
 //   std::cout << "Perimeter: " << triangle.calculatePerimeter() << std::endl;
+float perimetercircle = 2*3.14*7;
+float areacircle = 3.14*7*7;
+//std::cout <<"The perimeter of circle is: " << perimetercircle << std::endl;
+//std::cout <<"The area of circle is: " << areacircle << std::endl;
+
+float arearectangle = 4.2*8.0;
+float perimeterrectangle = 2*(4.2+8.0);
+//std::cout <<"The area of rectangle is: " << arearectangle << std::endl;
+//std::cout <<"The perimeter of rectangle is: " << perimeterrecangle << std::endl;
+
+float areatriangle = 1/2*4*3.6;
+float perimetertriangle = 4.0+4.0+3.2;
+//std::cout <<"The area of triangle is: " << areatriangle << std::endl;
+//std::cout <<"The perimeter of triangle is: " << perimetertriangle << std::endl;
 
   return 0;
 }
